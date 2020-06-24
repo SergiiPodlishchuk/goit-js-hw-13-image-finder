cc4342cc462fe9b909c7b6d7746450f0c4514233
SergiiPodlishchuk <podluy23@gmail.com>
2020-06-24 22:14:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.css';
+
+import { alert, notice, info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/Material.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
