9fc737b0ac09332642a10ebf9bd43e0c899c5925
SergiiPodlishchuk <podluy23@gmail.com>
2020-07-04 23:51:32 +0300

    hw13

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import * as basicLightbox from 'basiclightbox';
 import apiService from './js/apiService';
 const debounce = require('lodash.debounce');
 
@@ -7,7 +8,9 @@ import { alert, notice, info, success, error } from '@pnotify/core';
 import '@pnotify/core/dist/Material.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
-import * as basicLightbox from 'basiclightbox';
+import InfiniteScroll from 'infinite-scroll';
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
 
 const refs = {
   galleryUl: document.querySelector('#gallery'),
@@ -17,6 +20,7 @@ const refs = {
 
 refs.formInput.addEventListener('input', debounce(searchImages, 1000));
 refs.btnLoadMore.addEventListener('click', addMoreImages);
+refs.galleryUl.addEventListener('click', bigImage);
 
 function searchImages(e) {
   e.preventDefault();
@@ -36,9 +40,6 @@ function searchImages(e) {
   apiService.resetPage();
   apiService.searchImage = input;
   apiService.fetchAPI().then(data => {
-    const markup = imageListItem(data.hits);
-    refs.galleryUl.insertAdjacentHTML('beforeend', markup);
-
     if (data.hits.length === 0) {
       const myAlert = alert({
         text: `Your search did not match.`,
@@ -49,6 +50,10 @@ function searchImages(e) {
       refs.galleryUl.innerHTML = '';
       return;
     }
+
+    const markup = imageListItem(data.hits);
+    refs.galleryUl.insertAdjacentHTML('beforeend', markup);
+    masonry();
   });
 
   refs.galleryUl.innerHTML = '';
@@ -63,5 +68,63 @@ function addMoreImages() {
   apiService.fetchAPI().then(data => {
     const markup = imageListItem(data.hits);
     refs.galleryUl.insertAdjacentHTML('beforeend', markup);
+
+    masonry();
   });
 }
+
+function bigImage(e) {
+  const imageSrc = e.target.getAttribute('srcset');
+  const instance = basicLightbox.create(`<img width="1200" src="${imageSrc}">`);
+
+  const visible = basicLightbox.visible();
+  const img = document.querySelector('.basicLightbox');
+  console.log(e.target);
+
+  instance.show();
+
+  window.scrollTo({
+    top: refs.galleryUl.offsetHeight,
+    left: 0,
+    behavior: 'smooth',
+  });
+}
+
+function masonry() {
+  imagesLoaded('#gallery', function () {
+    const masonryInstance = new Masonry('#gallery', {
+      columnWidth: 350,
+      itemSelector: '.grid-item',
+      gutter: 5,
+      // percentPosition: true,
+      transitionDuration: '4s',
+    });
+  });
+}
+
+// const infScroll = new InfiniteScroll(refs.galleryUl, {
+//   path() {
+//     const options = {
+//       headers: {
+//         Authorization: '17198919-7cedd3f99d379df98db4093df',
+//       },
+//     };
+//     return `https://pixabay.com/api/?q=cat&image_type=photo&orientation=horizontal&page=${this.pageIndex}&per_page=12&key=17198919-7cedd3f99d379df98db4093df`;
+//   },
+//   append: '.post',
+//   history: false,
+//   responseType: 'text',
+// });
+
+// console.log(infScroll);
+
+// infScroll.on('load', (response, url) => {
+//   const images = JSON.parse(response);
+//   const markup = images.map(image => imageListItem(image).join(''));
+//   const proxyEl = document.createElement('div');
+//   proxyEl.innerHTML = markup;
+//   const parsedImages = proxyEl.querySelectorAll('.');
+//   infScroll.appendItems(parsedImages);
+// });
+
+// infScroll.loadNextPage();
